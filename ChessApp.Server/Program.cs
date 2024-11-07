f26840ddf6c7d7b01241a258df6c85d1be806556
DamianSubzda <DamianSubzda@wp.pl>
2024-11-07 13:10:29 +0100

    Initial project commit

---
+++ ChessApp.Server/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", builder =>
+        builder.WithOrigins("https://localhost:3000", "http://localhost:3000")
+               .AllowAnyHeader()
+               .AllowAnyMethod());
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowReactApp");
+app.UseStaticFiles();
+
+app.Run();
