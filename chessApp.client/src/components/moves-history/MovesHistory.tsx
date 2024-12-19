cd2374eceb8d8d459f4bb4f6b19904a0eaf17d16
DamianSubzda <DamianSubzda@wp.pl>
2024-12-19 09:18:34 +0100

    Moves history styling

--- chessApp.client/src/components/moves-history/MovesHistory.tsx
+++ chessApp.client/src/components/moves-history/MovesHistory.tsx
@@ -5,27 +5,32 @@ import { AppState } from "../../store/store";
 
 function MovesHistory() {
     const moves = useSelector((state: AppState) => state.movesHistory.moves);
-    
+
     return (
         <div className="moves-history-container">
-            <h3>Moves:</h3>
             <div className="table-container">
                 <table className="table__element">
                     <thead>
                         <tr>
-                            <th style={{width: "10%"}}>Lp.</th>
-                            <th style={{width: "45%"}}>White</th>
-                            <th style={{width: "45%"}}>Black</th>
+                            <th>Lp.</th>
+                            <th>White</th>
+                            <th>Black</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {moves.map((move, index) => (
-                            <tr key={index}>
-                                <td>{index + 1}</td>
-                                <td>{move.white}</td>
-                                {move.black && <td>{move.black}</td>}
+                        {moves.length === 0 ? (
+                            <tr>
+                                <td colSpan={3} className="no-moves">No moves recorded...</td>
                             </tr>
-                        ))}
+                        ) : (
+                            moves.map((move, index) => (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{move.white}</td>
+                                    <td>{move.black || "-"}</td>
+                                </tr>
+                            ))
+                        )}
                     </tbody>
                 </table>
             </div>
