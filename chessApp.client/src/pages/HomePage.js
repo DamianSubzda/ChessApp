e9b04eb93b67b4973db3996c3ddcf3a032548b49
Damian Subzda <d.subzda@inforsys.pl>
2025-02-03 12:28:00 +0100

    Styled Home page

--- chessApp.client/src/pages/HomePage.js
+++ chessApp.client/src/pages/HomePage.js
@@ -15,10 +15,17 @@ function HomePage(){
     }
 
     return(
-        <div className="home-page">
-            <div className="panel">
-                <button id="button" onClick={() => handleRedirect("/new-game")}>New Game</button>
-                <button id="button" onClick={() => handleRedirect("/lobby")}>Lobby</button>
+        <div className="home__page">
+            <div className="home__panel">
+                <div className="home__panel__title">
+                    <img src="/assets/pieces/neo/wp.png" alt="" />
+                    <h1>Chess</h1>
+                    <img src="/assets/pieces/neo/bp.png" alt="" />
+                </div>
+                <div className="home__panel__menu">
+                    <button id="button" onClick={() => handleRedirect("/new-game")}>New Game</button>
+                    <button id="button" onClick={() => handleRedirect("/lobby")}>Lobby</button>
+                </div>
             </div>
         </div>
     );
