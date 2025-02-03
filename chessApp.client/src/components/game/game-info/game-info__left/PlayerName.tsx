6865e38e681fd053f4944c708ca28cdd3a4917f3
Damian Subzda <d.subzda@inforsys.pl>
2025-02-03 12:19:58 +0100

    Styled PlayerName component

--- chessApp.client/src/components/game/game-info/game-info__left/PlayerName.tsx
+++ chessApp.client/src/components/game/game-info/game-info__left/PlayerName.tsx
@@ -1,15 +1,12 @@
 import React from "react";
+import "./PlayerName.scss"
 
 interface PlayerNameProps {
   name: string | undefined;
 }
 
 function PlayerName({ name }: PlayerNameProps) {
-  return (
-    <>
-      <p>{name}</p>
-    </>
-  );
+  return <p className="player-name">{name}</p>;
 }
 
 export default PlayerName;
